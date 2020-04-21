28b382807228f2574f8a454c60dad38d30d4136e
ahmed sayed <e.eng.ahmedsayed@gmail.com>
2020-04-22 00:19:30 +0200

    init project & add tailwind and livewire

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Ludo</title>
+
+
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+</head>
+
+<body>
+
+</body>
+
+</html>
