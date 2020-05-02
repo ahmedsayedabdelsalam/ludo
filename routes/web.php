8ecad08b08dbd08b292e58480b5a118d4c4e8978
ahmed sayed <e.eng.ahmedsayed@gmail.com>
2020-05-02 12:36:42 +0200

    add game board (html & css)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('snakes-and-ladders', function () {
+    return view('snakes-and-ladders');
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,7 @@ const tailwindcss = require('tailwindcss')
 
 mix.js('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/snakes-and-ladders.scss', 'public/css')
   .options({
     processCssUrls: false,
     postCss: [tailwindcss('./tailwind.config.js')],
