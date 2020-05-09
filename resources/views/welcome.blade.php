5017830a638c7e11ba1d6db9f27ee1650fb020f1
ahmed sayed <e.eng.ahmedsayed@gmail.com>
2020-05-09 17:23:21 +0200

    play with alpine js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,105 +13,34 @@
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 
-<body class="bg-teal-700">
-    <div class="container mx-auto">
-        <div class="  bg-white  grid grid-rows-5">
-            <div class="row-span-2 grid   grid-cols-5">
-                <div class="bg-green-500 col-span-2 border-solid border-4 border-gray-900 p-16">
-                    <div class=" bg-white border-4 border-gray-900 grid grid-rows-2">
-                        <div class="grid  grid-cols-2">
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900  bg-green-500"></div>
-                            </div>
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900 bg-green-500"></div>
-                            </div>
-                        </div>
-                        <div class="grid  grid-cols-2">
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900 bg-green-500"></div>
-                            </div>
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900 bg-green-500"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-span-1">
+<body class="bg-teal-700 text-white">
+<div x-data="{ open: false }">
+    <button @click="open = true">Open Dropdown</button>
 
-                </div>
-                <div class="bg-yellow-500 col-span-2 border-solid border-4 border-gray-900 p-16">
-                    <div class=" bg-white border-4 border-gray-900 grid grid-rows-2">
-                        <div class="grid  grid-cols-2">
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900  bg-yellow-500"></div>
-                            </div>
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900 bg-yellow-500"></div>
-                            </div>
-                        </div>
-                        <div class="grid  grid-cols-2">
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900 bg-yellow-500"></div>
-                            </div>
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900 bg-yellow-500"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row-span-1">
-            </div>
-            <div class="row-span-2 grid  grid-cols-5">
-                <div class="bg-red-500 col-span-2 border-solid border-4 border-gray-900 p-16">
-                    <div class=" bg-white border-4 border-gray-900 grid grid-rows-2">
-                        <div class="grid  grid-cols-2">
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900  bg-red-500"></div>
-                            </div>
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900 bg-red-500"></div>
-                            </div>
-                        </div>
-                        <div class="grid  grid-cols-2">
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900 bg-red-500"></div>
-                            </div>
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900 bg-red-500"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class=" col-span-1">
+    <ul
+        x-show="open"
+        @click.away="open = false"
+        class="bg-gray-900"
+    >
+        Dropdown Body
+    </ul>
+</div>
 
-                </div>
-                <div class="bg-blue-500 col-span-2 border-solid border-4 border-gray-900 p-16">
-                    <div class=" bg-white border-4 border-gray-900 grid grid-rows-2">
-                        <div class="grid  grid-cols-2">
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900  bg-blue-500"></div>
-                            </div>
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900 bg-blue-500"></div>
-                            </div>
-                        </div>
-                        <div class="grid  grid-cols-2">
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900 bg-blue-500"></div>
-                            </div>
-                            <div class=" flex">
-                                <div class="rounded-full h-16 w-16 m-auto border-4 border-gray-900 bg-blue-500"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div x-data="{name: null}">
+    <label>
+        <input type="text" x-model="name">
+    </label>
+    <div x-text="name"></div>
+</div>
 
-        </div>
-    </div>
+<div x-data="{counter: 0}">
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+    <span x-text="counter"></span>
+</div>
 
+<!-- scripts -->
+<script src="{{mix('js/app.js')}}"></script>
 </body>
 
 </html>
